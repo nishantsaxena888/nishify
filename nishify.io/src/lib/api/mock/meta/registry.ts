c6562ad528c8cfcb88db784b2db6ce9bbf74dced
Nishant Saxena <nishantsaxena@Nishants-MacBook-Pro.local>
2025-08-01 20:59:02 -0400

    Latest

--- nishify.io/src/lib/api/mock/meta/registry.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import menuOptions from "./menu.options";
-// import other mock options as needed
-
-export const mockOptionModules: Record<string, any> = {
-  menu: menuOptions,
-  // Add other entities here
-};
